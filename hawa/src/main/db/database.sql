b0b7be3eb909f481ec20a5f2bba2bf4f83714c2f
PoJD <luboshousa@gmail.com>
2014-09-17 12:14:44 +0200

    fixing issues to be able to deploy again to Rpi after all changes (copy paste error in sql properties and grant to rpi directly too in mysql)

--- hawa/src/main/db/database.sql
+++ hawa/src/main/db/database.sql
@@ -1,3 +1,4 @@
 create database hawa;
 grant usage on *.* to 'superuser'@'%' identified by 'superStrong123Password';
+grant usage on *.* to 'superuser'@'rpi' identified by 'superStrong123Password';
 grant all privileges on hawa.* to 'superuser'@'%';
